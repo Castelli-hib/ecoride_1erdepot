01d86643ccf78db58a8d94a36ebd93457a075c41
NadmhaMcalli <marie.castelli@adiktionstudio.com>
2025-09-04 20:53:46 +0200

    creation du token et mailHog

--- app/src/Controller/RegistrationController.php
+++ app/src/Controller/RegistrationController.php
@@ -6,19 +6,25 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use App\Entity\User;
 use App\Form\RegistrationFormType;
 use Doctrine\ORM\EntityManagerInterface;
+// use Symfony\Bundle\SecurityBundle\Security\UserAuthenticator;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
+use App\Service\JWTService;
+use App\Security\UserAuthenticator; 
 
 class RegistrationController extends AbstractController
 {
     #[Route('/register', name: 'app_register')]
-    public function register(
-        Request $request, 
-        EntityManagerInterface $entityManager, 
-        UserPasswordHasherInterface $userPasswordHasher
-    ): Response
+    public function register(Request $request, 
+            UserPasswordHasherInterface $userPasswordHasher, 
+            UserAuthenticatorInterface $userAuthenticator, 
+            UserAuthenticator $authenticator, 
+            EntityManagerInterface $entityManager, 
+            JWTService $jwt
+        ): Response
     {
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
@@ -42,9 +48,29 @@ class RegistrationController extends AbstractController
             // do anything else you need here, like send an email
 
             //Générer le token JWT
+            //Header
+            $header = [
+                'typ' => 'JWT',
+                'alg' => 'HS256'
+            ];
+
+            //Payload
+            $payload = [
+                'user_id' => $user->getId(),
+                'email' => $user->getEmail() 
+            ];
+
+            //On génère le token
+            $token = $jwt->generate($header, $payload, $this->getParameter('app.jwtsecret'));
             
-            
+            dd($token);
+
             //envoyer par email
+            return $userAuthenticator->authenticatorUser(
+                $user,
+                $authenticator,
+                $request
+            );
 
             // redirection vers login
             return $this->redirectToRoute('app_login');
