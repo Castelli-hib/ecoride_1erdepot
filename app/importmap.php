c9ff07f25f0b9646fa9f7e3c181d0a92f6e47c9c
NadmhaMcalli <marie.castelli@adiktionstudio.com>
2025-10-13 14:25:08 +0200

    Normalisation des fins de ligne avec .gitattributes Nettoyage du suivi Git et mise à jour du .gitignore

--- app/importmap.php
+++ app/importmap.php
@@ -16,11 +16,15 @@ return [
         'path' => './assets/app.js',
         'entrypoint' => true,
     ],
+    'register' => [
+        'path' => 'assets/js/register.js',
+        'entrypoint' => true,
+    ],
     '@hotwired/stimulus' => [
         'version' => '3.2.2',
     ],
     '@symfony/stimulus-bundle' => [
-        'path' => './vendor/symfony/stimulus-bundle/assets/dist/loader.js',
+        'path' => 'vendor/symfony/stimulus-bundle/assets/dist/loader.js',
     ],
     '@hotwired/turbo' => [
         'version' => '7.3.0',
